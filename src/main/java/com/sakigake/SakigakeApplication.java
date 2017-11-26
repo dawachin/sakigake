f10c89992411bd29ed5efa8722f288a4445ea687
大和田裕晃 <hiroakiohwada@Hiroaki-Ohwada.local>
2017-11-26 21:49:35 +0900

    Initial commit

---
+++ src/main/java/com/sakigake/SakigakeApplication.java
@@ -0,0 +1,12 @@
+package com.sakigake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakigakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakigakeApplication.class, args);
+	}
+}
